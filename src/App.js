555988614ae2b1ff79b899d9cf9cf42ea82e6118
Abdullah Al Sohel <absohel47@gmail.com>
2022-11-26 21:04:36 +0600

    React Component Basic

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ function App() {
   return (
     <div className='App'>
       <h1>Hello</h1>
+      <Person />
     </div>
   );
+}
+
+function Person() {
+  return (
+    <div>
+      <h1>I am person component</h1>
+    </div>
+  )
 }
 
 export default App;
