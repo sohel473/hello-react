ff83a19d461d051a436d40bfb9c5a491a29ec8d3
Sohel <absohel47@gmail.com>
2023-01-24 02:22:04 +0000

    Controlled Form

--- src/components/representational/NewBook.js
+++ src/components/representational/NewBook.js
@@ -1,12 +1,68 @@
-import React from "react";
+import React, { Component } from "react";
 
-const NewBook = (props) => {
+class NewBook extends Component {
   // console.log(props);
-  return (
-    <div>
-      <h1>New Book Entry</h1>
-    </div>
-  );
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookName: "",
+      writer: "",
+      description: "",
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit = (event) => {
+    console.log(this.state);
+    event.preventDefault();
+  };
+
+  handleInputChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>New Book Entry</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>Book Name: </label>
+          <br />
+          <input
+            type="text"
+            name="bookName"
+            value={this.state.bookName}
+            onChange={this.handleInputChange}
+          />
+          <br />
+          <label>Writer: </label>
+          <br />
+          <input
+            type="text"
+            name="writer"
+            value={this.state.writer}
+            onChange={this.handleInputChange}
+          />
+          <br />
+          <label>Description: </label>
+          <br />
+          <textarea
+            name="description"
+            value={this.state.description}
+            onChange={this.handleInputChange}
+          />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
 
 export default NewBook;
